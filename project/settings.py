771a9e0a90d5e1645d9b1bf310ccefddc3f61195
MAIELMELIGY <mairshad238@gmail.com>
2021-10-22 11:15:20 +0200

    fix bug

--- project/settings.py
+++ project/settings.py
@@ -15,10 +15,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5%i)hi$$&zs@282ecrt*t*$fcmqod%j^9!-k^@hnbdi^$j2au3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG =True
 
 
-ALLOWED_HOSTS = ['apihomeautomation.herokuapp.com', '127.0.0.1:8000','197.59.33.53']
+#ALLOWED_HOSTS = ['apihomeautomation.herokuapp.com', '127.0.0.1:8000','197.59.33.53']
+ALLOWED_HOSTS = ['*']
 
 
 
